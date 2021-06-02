424151906d3143c735c009fc0aaf2f4d2c9031c3
Claudio Iury <iurylemos4@gmail.com>
2021-06-02 18:01:15 -0300

    refactoring code using redux saga to listen when data change

--- src/store/modules/reserve/reducer.ts
+++ src/store/modules/reserve/reducer.ts
@@ -6,17 +6,7 @@ function reserve(state: Array<any> = [], action: AnyAction): Array<any> {
     switch (action.type) {
         case 'ADD_RESERVE_SUCCESS':
             return produce(state, (draft: any[]) => {
-                const tripIndex = draft.findIndex(trip => trip.id === action.trip.id);
-
-                if (tripIndex >= 0) {
-                    draft[tripIndex].amount += 1;
-                } else {
-                    draft.push({
-                        ...action.trip,
-                        amount: 1,
-                    });
-                }
-
+                draft.push(action.trip);
             });
         case 'REMOVE_RESERVE':
             return produce(state, (draft: any[]) => {
--- src/store/modules/reserve/sagas.ts
+++ src/store/modules/reserve/sagas.ts
@@ -1,16 +1,37 @@
 import { AxiosResponse } from 'axios';
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import { api } from '../../../services/api_service';
-import { addReserveSuccess } from './actions';
+import { addReserveSuccess, updateReserve } from './actions';
 
 type Params = {
     tripId: number
 }
 
+export interface TripSaga {
+    id: number;
+    title: string;
+    status: boolean;
+    image: string;
+    amount: number;
+}
+
+
 function* addToReserve({ tripId }: Params) {
     try {
-        const response: AxiosResponse = yield call(api.get, `trips/${tripId}`);
-        yield put(addReserveSuccess(response.data));
+        const tripExist: TripSaga = yield select(state => state.reserve.find((trip: TripSaga) => trip.id === tripId))
+        if (tripExist) {
+            const amount = tripExist.amount + 1;
+            yield put(updateReserve(tripExist.id, amount));
+        } else {
+            const response: AxiosResponse = yield call(api.get, `trips/${tripId}`);
+
+            const data = {
+                ...response.data,
+                amount: 1,
+            };
+
+            yield put(addReserveSuccess(data));
+        }
     } catch (error) {
 
     }
